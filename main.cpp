d3165135967a1afd0e8d89b18b0ba8776bfcdda6
Bruno-Colturato <brcolturato@usp.br>
2021-11-08 13:21:37 -0300

    Comentando main

--- main.cpp
+++ main.cpp
@@ -37,22 +37,30 @@ int verifica_vitoria(char vitoria, Cliente*player)
 
 
 int main(int argc, char *argv[])
-{    
+{
+    // Criando variáveis uteis para a lógica do jogo
+    char verifica_vitoria_char = '-';
+    int count_sleep=0;
+    char sleep;
+    
+    // Obtendo nome do jogador
+    string Nome;
+    std::cout << "Digite seu Nome: " << std::endl;
+    std::getline(cin,Nome);
+
+    // Criando objeto responsável pela comunicação do jogo com o servidor
+    Cliente* player = new Cliente(Nome);
+
+    // Criando threads e variáveis uteis para manipulação do player/cliente
     std::thread aguarda_player2;
     std::thread recebe_tabuleiro;
     std::thread envia_tabuleiro;
     bool wait_for_player2 = false;
     bool iniciou_jogo = false;
-    char verifica_vitoria_char = '-';
-    char sleep;
-    int count_sleep=0;
-    
-    string Nome;
-    std::cout << "Digite seu Nome" << std::endl;
-    std::getline(cin,Nome);
 
-    Cliente* player = new Cliente(Nome);
+    /** Iniciando lógica da parte gráfica **/
 
+    // Variáveis que guarda qual janela deve ser exibida
     int JanelaAtual = 1;
 
     // Criando configuracoes de contexto para ter formas com melhores contornos
@@ -67,41 +75,47 @@ int main(int argc, char *argv[])
 
     // Importando icone da aplicacao
     sf::Image icon;
-    if(!icon.loadFromFile("texturas/Icon.png")) {
-        std::cout << "vixi" << std::endl;
+    if(!icon.loadFromFile("texturas/Icon.png"))
+    {
+        std::cout << "Erro ao obter ícone" << std::endl;
     }
-
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
-    bool mouse_clicado = false;
+    // Configurando variáveis pra controle do clique do mouse
     sf::Vector2i Mouse_clique_position;
     sf::Vector2i Mouseposition;
+    bool mouse_clicado = false;
 
-
-    TelaLogin* tela_login = new TelaLogin(Nome); //1
-    TelaInstrucao* tela_instrucao = new TelaInstrucao(); //2
-    TelaAguardo* tela_aguardo = new TelaAguardo(); //3
-    TelaJogo* tela_jogo;                        //4
-    TelaVitoria* tela_vitoria = new TelaVitoria();  //5
-    TelaDerrota* tela_derrota = new TelaDerrota();  //6
+    // Criando objetos para manipulação das telas do jogo
+    TelaLogin* tela_login = new TelaLogin(Nome);            //1
+    TelaInstrucao* tela_instrucao = new TelaInstrucao();    //2
+    TelaAguardo* tela_aguardo = new TelaAguardo();          //3
+    TelaJogo* tela_jogo;                                    //4
+    TelaVitoria* tela_vitoria = new TelaVitoria();          //5
+    TelaDerrota* tela_derrota = new TelaDerrota();          //6
     TelaDisconnect* tela_disconnect = new TelaDisconnect(); //7
 
+    /** Gameloop **/
 
     // Rodar o codigo enquanto a janela estiver aberta (game loop)
-    while(window.isOpen()) {
+    while(window.isOpen()) 
+    {
 
         // Verificar todos o eventos que ocorreram durante o ultimo loop
         sf::Event event;
-        while(window.pollEvent(event)) {
+
+        while(window.pollEvent(event)) 
+        {
             // Verificar o evento de fechar a janela
             if(event.type == sf::Event::Closed) {
                 std::cout << "Execucao finalizada" << std::endl;
                 window.close();
             }
-            // Verificar clique do mouse (botao esquerdo)
-            if(event.type == sf::Event::MouseButtonPressed) {
-                if(event.mouseButton.button == sf::Mouse::Left) {
-                    
+            // Verificar clique do mouse (botão esquerdo)
+            if(event.type == sf::Event::MouseButtonPressed)
+            {
+                if(event.mouseButton.button == sf::Mouse::Left)
+                {
                     mouse_clicado = true;
                     Mouse_clique_position.x = event.mouseButton.x;
                     Mouse_clique_position.y = event.mouseButton.y;
@@ -109,10 +123,13 @@ int main(int argc, char *argv[])
             }
         }
 
+        /** Tratamento dos dados dos eventos **/
+
+        // Posição do mouse
         Mouseposition = sf::Mouse::getPosition(window);
 
         // Desenho de elementos
-        // Limpar tela (obrigatorio)
+        // Limpar tela (obrigatório)
         window.clear(sf::Color(255,255,255));
 
         //Conecta no servidor
@@ -122,58 +139,98 @@ int main(int argc, char *argv[])
         }
 
         // Cria thread para aguardar conexão do player 2
-        
         if(JanelaAtual==3 && !wait_for_player2)
         {
             aguarda_player2 = player->thread_aguarda_jogador();
             wait_for_player2=true;
         }
 
-        if(player->player2_conectado  && !iniciou_jogo) 
+        // Verifica se devemos redirecionar a tela do jogador para a tela de jogo
+        if(player->player2_conectado && !iniciou_jogo) 
         {
             JanelaAtual = 4; //Jogo
         }       
         
-        if(JanelaAtual == 4 && !iniciou_jogo) {
-            tela_jogo = new TelaJogo(player->tipo_jogador,player->Nome,player->nome_adversario);
+        // Na primeira vez que o jogador clica para jogar, criamos as threads para comunicação
+        // com o servidor
+        if(JanelaAtual == 4 && !iniciou_jogo)
+        {
+            // Criando objeto de tela de jogo
+            tela_jogo = new TelaJogo(player->tipo_jogador, player->Nome, player->nome_adversario);
+
+            // Inicialiando threads
             envia_tabuleiro = player->thread_envia_tabuleiro();
             recebe_tabuleiro = player->thread_recebe_tabuleiro();
+
             iniciou_jogo = true;
         }
         
+        // Verificando qual janela deve ser desenhada na tela e tratando dados
         switch(JanelaAtual)
         {
             case 1:
+                // Comunica que a tela deve monitorar o mouse
                 tela_login->ReactToMouse(Mouseposition);
+
+                // Desenha a tela na janela do jogo
                 window.draw(*tela_login);
-                if(mouse_clicado) JanelaAtual = tela_login->mouseclique(Mouse_clique_position);
+
+                // Determinando próxima tela com base no clique do mouse
+                if(mouse_clicado)
+                {
+                    JanelaAtual = tela_login->mouseclique(Mouse_clique_position);
+                }
                 break;
             case 2:
+                // Comunica que a tela deve monitorar o mouse
                 tela_instrucao->ReactToMouse(Mouseposition);
+
+                // Desenha a tela na janela do jogo
                 window.draw(*tela_instrucao);
-                if(mouse_clicado) JanelaAtual = tela_instrucao->mouseclique(Mouse_clique_position);
+
+                // Determinando próxima tela com base no clique do mouse
+                if(mouse_clicado)
+                {
+                    JanelaAtual = tela_instrucao->mouseclique(Mouse_clique_position);
+                }
                 break;
             case 3:
+                // Animação de texto
                 tela_aguardo->change_text();
+
+                // Comunica que a tela deve monitorar o mouse
                 tela_aguardo->ReactToMouse(Mouseposition);
+
+                // Desenha a tela na janela do jogo
                 window.draw(*tela_aguardo);
-                if(mouse_clicado) JanelaAtual = tela_aguardo->mouseclique(Mouse_clique_position);
-                //Aguarda Confirmacao
-                
+
+                // Determinando próxima tela com base no clique do mouse
+                if(mouse_clicado) {
+                     JanelaAtual = tela_aguardo->mouseclique(Mouse_clique_position);
+                }                
                 break;
             case 4:
+                // Imprimindo nome do jogador do turno atual
                 tela_jogo->set_nome_turno(player->turno);
+                
+                // Monitorando o clique do mouse
                 if(mouse_clicado) {
                     player->clicado = tela_jogo->mouseclique(Mouse_clique_position, player,player->turno);
                 }
+
+                // Monitorando a atualização do tabuleiro vindo do servidor
                 if(player->recebeu)
                 {
                     verifica_vitoria_char = tela_jogo->set_tabuleiro(player);
                     player->recebeu = false;
                 }
+
+                // Desenha a tela na janela do jogo
                 window.draw(*tela_jogo);
+
+                // Verificando vitória e dando sleep na tela
                 sleep = verifica_vitoria(verifica_vitoria_char, player);
-                if(player->turno >50 && sleep!=5 && sleep !=6)
+                if(player->turno > 50 && sleep!=5 && sleep !=6)
                 {
                     JanelaAtual = 7;
                 }
@@ -187,12 +244,15 @@ int main(int argc, char *argv[])
                 }
                 break;
             case 5:
+                // Desenha a tela na janela do jogo
                 window.draw(*tela_vitoria);
                 break;
             case 6:
+                // Desenha a tela na janela do jogo
                 window.draw(*tela_derrota);
                 break;
             case 7:  
+                // Desenha a tela na janela do jogo
                 window.draw(*tela_disconnect);
                 break;
 
@@ -204,6 +264,7 @@ int main(int argc, char *argv[])
 
         if(mouse_clicado) mouse_clicado = false;
 
+        // Exibe a janela
         window.display();
     }
     return 0;
